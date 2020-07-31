b3b0fffdfdfe755d1f445bb5bec2e471eb08856c
Travis Howard <travis.howard.tj@gmail.com>
2020-07-31 00:39:26 -0400

    backup shell script fixes

--- backups/cloud/zcloud_back.sh
+++ backups/cloud/zcloud_back.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 AMI=`cat /home/oskr_grme/.local/ami_id.txt`
 MYIP=`dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}'`
-aws cloudformation create-stack --stack-name zcloud --template-body file://home/oskr_grme/.local/zcloud.cfn.yml --parameters ParameterKey=AMI,ParameterValue=$AMI ParameterKey=MYIP,ParameterValue=$MYIP
-sleep 500
-INSTANCE=`aws cloudformation describe-stack-resource --stack-name zcloud --logical-resource-id ZFSInstance | jq '.StackResourceDetail.PhysicalResourceId'`
-IP=`aws ec2 describe-instances --instances $INSTANCE --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
-syncoid zroot ubuntu@$IP:zcloud/zroot
-aws ec2 create-image --instance-id $INSTANCE --name "zcloud-$(date -I)" --description "Zcloud syncoid replication target, built on $(date -I'minutes')" | jq '.ImageId' > /home/oskr_grme/.local/ami_id.txt
+aws --profile zfs cloudformation create-stack --stack-name zcloud --template-body file:///home/oskr_grme/.local/zcloud.zfn.yml --parameters ParameterKey=AMI,ParameterValue=$AMI ParameterKey=MYIP,ParameterValue=$MYIP
+sleep 300
+INSTANCE=`aws --profile zfs cloudformation describe-stack-resource --stack-name zcloud --logical-resource-id ZFSInstance | jq '.StackResourceDetail.PhysicalResourceId' | sed -e 's/^"//' -e 's/"$//'`
+IP=`aws --profile zfs ec2 describe-instances --instance-ids $INSTANCE --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
+syncoid --sshkey=/home/oskr_grme/Downloads/new-zfs.pem --sshoption=StrictHostKeyChecking=no zroot ubuntu@$IP:zcloud/zroot
+aws --profile zfs ec2 create-image --instance-id $INSTANCE --name "zcloud-$(date -I)" --description "Zcloud syncoid replication target, built on $(date -I'minutes')" | jq '.ImageId' | sed -e 's/^"//' -e 's/"$//'> /home/oskr_grme/.local/ami_id.txt
 sleep 60
-aws cloudformation delete-stack --stack-name zcloud
+aws --profile zfs cloudformation delete-stack --stack-name zcloud
 echo "ZFS replication to AWS successfully completed on $(date -I'minutes')" >> /home/oskr_grme/.local/logs/zcloud.log
