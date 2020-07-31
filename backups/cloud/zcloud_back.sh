041c09474e4c14285eb97bc4fcebc67aec137841
OskergMonReap <travis.howard.tj@gmail.com>
2020-07-31 12:21:19 -0400

    add regex checks for ami and instance ids

--- backups/cloud/zcloud_back.sh
+++ backups/cloud/zcloud_back.sh
@@ -2,14 +2,14 @@
 LOGFILE='/home/oskr_grme/.local/logs/zcloud.log'
 AMI=`cat /home/oskr_grme/.local/ami_id.txt`
 MYIP=`dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}'`
-if [ -n $AMI ]
+if [[ $AMI =~ ^(ami)-[a-zA-Z0-9]*$ ]]
 then
     echo "Valid AMI ID collected: $AMI" >> $LOGFILE
 else
     echo "Invalid AMI ID, please check ami_id.txt: $AMI" >> $LOGFILE
     exit 1
 fi
-if [ $MYIP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]
+if [[ $MYIP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
 then
     echo "Valid IP address for $HOSTNAME: $MYIP" >> $LOGFILE
 else
@@ -21,14 +21,14 @@ aws --profile zfs cloudformation create-stack --stack-name $HOSTNAME-zcloud --te
 sleep 300
 INSTANCE=`aws --profile zfs cloudformation describe-stack-resource --stack-name $HOSTNAME-zcloud --logical-resource-id ZFSInstance | jq '.StackResourceDetail.PhysicalResourceId' | sed -e 's/^"//' -e 's/"$//'`
 IP=`aws --profile zfs ec2 describe-instances --instance-ids $INSTANCE --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
-if [ -n $INSTANCE ]
+if [[ $INSTANCE =~ ^i-[a-zA-Z0-9]*$ ]]
 then
     echo "Valid ID from ec2 instance found: $INSTANCE" >> $LOGFILE
 else
     echo "Invalid ID for ec2 instance: $INSTANCE" >> $LOGFILE
     exit 1
 fi
-if [ $IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]
+if [[ $IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
 then
     syncoid --sshkey=/home/oskr_grme/Downloads/new-zfs.pem --sshoption=StrictHostKeyChecking=no zroot ubuntu@$IP:zcloud/zroot > $LOGFILE 2>&1
 else
