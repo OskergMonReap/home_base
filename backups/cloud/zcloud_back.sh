e80656039bdcde689fbe5e8ae2454b0c6b2ae1e1
OskergMonReap <travis.howard.tj@gmail.com>
2020-08-01 11:02:47 -0400

    fix redirects, adjust echos

--- backups/cloud/zcloud_back.sh
+++ backups/cloud/zcloud_back.sh
@@ -4,38 +4,39 @@ AMI=`cat /home/oskr_grme/.local/ami_id.txt`
 MYIP=`dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}'`
 if [[ $AMI =~ ^(ami)-[a-zA-Z0-9]*$ ]]
 then
-    echo "Valid AMI ID collected: $AMI" >> $LOGFILE
+    echo "INFO: Valid AMI ID collected: $AMI" >> $LOGFILE
 else
-    echo "Invalid AMI ID, please check ami_id.txt: $AMI" >> $LOGFILE
+    echo "ERROR: Invalid AMI ID, please check ami_id.txt: $AMI" >> $LOGFILE
     exit 1
 fi
 if [[ $MYIP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
 then
-    echo "Valid IP address for $HOSTNAME: $MYIP" >> $LOGFILE
+    echo "INFO: Valid IP address for $HOSTNAME: $MYIP" >> $LOGFILE
 else
-    echo "Invalid IP address for $HOSTNAME: $MYIP" >> $LOGFILE
+    echo "ERROR: Invalid IP address for $HOSTNAME: $MYIP" >> $LOGFILE
     exit 1
 fi
-echo "Starting ZFS replication to AWS: $(date -I'minutes')"
-aws --profile zfs cloudformation create-stack --stack-name $HOSTNAME-zcloud --template-body file:///home/oskr_grme/.local/zcloud.zfn.yml --parameters ParameterKey=AMI,ParameterValue=$AMI ParameterKey=MYIP,ParameterValue=$MYIP > $LOGFILE 2>&1
+echo "INFO: Starting ZFS replication to AWS: $(date -I'minutes')" >> $LOGFILE
+aws --profile zfs cloudformation create-stack --stack-name $HOSTNAME-zcloud --template-body file:///home/oskr_grme/.local/zcloud.cfn.yml --parameters ParameterKey=AMI,ParameterValue=$AMI ParameterKey=MYIP,ParameterValue=$MYIP >> $LOGFILE 2>&1
 sleep 300
 INSTANCE=`aws --profile zfs cloudformation describe-stack-resource --stack-name $HOSTNAME-zcloud --logical-resource-id ZFSInstance | jq '.StackResourceDetail.PhysicalResourceId' | sed -e 's/^"//' -e 's/"$//'`
 IP=`aws --profile zfs ec2 describe-instances --instance-ids $INSTANCE --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
 if [[ $INSTANCE =~ ^i-[a-zA-Z0-9]*$ ]]
 then
-    echo "Valid ID from ec2 instance found: $INSTANCE" >> $LOGFILE
+    echo "INFO: Valid ID from ec2 instance found: $INSTANCE" >> $LOGFILE
 else
-    echo "Invalid ID for ec2 instance: $INSTANCE" >> $LOGFILE
+    echo "ERROR: Invalid ID for ec2 instance: $INSTANCE" >> $LOGFILE
     exit 1
 fi
 if [[ $IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
 then
-    syncoid --sshkey=/home/oskr_grme/Downloads/new-zfs.pem --sshoption=StrictHostKeyChecking=no zroot ubuntu@$IP:zcloud/zroot > $LOGFILE 2>&1
+    syncoid -R --sshkey=/home/oskr_grme/.ssh/zfsbackup.pem --sshoption=LogLevel=ERROR --sshoption=UserKnownHostsFile=/dev/null --sshoption=StrictHostKeyChecking=no zroot ubuntu@$IP:zcloud/zroot >> $LOGFILE 2>&1
 else
-    echo "Invalid IP address for EC2 instance: $IP" >> $LOGFILE
+    echo "ERROR: Invalid IP address for EC2 instance: $IP" >> $LOGFILE
     exit 1
 fi
 aws --profile zfs ec2 create-image --instance-id $INSTANCE --name "zcloud-$HOSTNAME-$(date -I)" --description "Zcloud syncoid replication target, built on $(date -I'minutes') for $HOSTNAME" | jq '.ImageId' | sed -e 's/^"//' -e 's/"$//'> /home/oskr_grme/.local/ami_id.txt
 sleep 60
-aws --profile zfs cloudformation delete-stack --stack-name $HOSTNAME-zcloud > $LOGFILE 2>&1
-echo "ZFS replication to AWS successfully completed on $(date -I'minutes')" >> $LOGFILE
+aws --profile zfs cloudformation delete-stack --stack-name $HOSTNAME-zcloud >> $LOGFILE 2>&1
+echo "INFO: ZFS replication to AWS successfully completed on $(date -I'minutes')" >> $LOGFILE 2>&1
+echo "-------------------------------------------" >> $LOGFILE 2>&1
